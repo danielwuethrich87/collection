abc9a13197b34b69854f53494595d96d1be64698
danielwuethrich87 <danielwue@hotmail.com>
2017-09-28 13:57:24 +0200

    Update convert.sh
    added RAxML

--- pipelines/roary/convert.sh
+++ pipelines/roary/convert.sh
@@ -1,1 +1,3 @@
 https://github.com/bioperl/bioperl-live/blob/master/scripts/Bio-DB-GFF/bp_genbank2gff3.pl
+raxmlHPC-PTHREADS-SSE3 -T "$NSLOTS" -x 1522 -f a -m GTRGAMMA -p 1522 -# 1000 -s ../core_gene_alignment.aln -n Alignment
+raxmlHPC-PTHREADS-SSE3 -T "$NSLOTS" -x 1522 -f a -m PROTGAMMAWAG -p 1522 -# 1000 -s ../to_use -n Alignment

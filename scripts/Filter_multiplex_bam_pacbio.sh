851ac00b0ac01c13f3f72aa1e478d90b004a9236
danielwuethrich87 <danielwue@hotmail.com>
2017-10-10 15:31:57 +0200

    Update Filter_multiplex_bam_pacbio.sh

--- scripts/Filter_multiplex_bam_pacbio.sh
+++ scripts/Filter_multiplex_bam_pacbio.sh
@@ -1,1 +1,1 @@
-./bamtools filter -tag "bq":">=20" -in ../../p2500_o3506_1_pool1.barcode.1.bam -out test.bam
+./bamtools filter -tag "bq":">=45" -in ../../p2500_o3506_1_pool1.barcode.1.bam -out test.bam
